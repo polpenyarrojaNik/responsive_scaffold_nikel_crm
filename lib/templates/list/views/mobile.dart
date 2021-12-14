01efcf5a05f7bc3a990e4b4edf7b836856ccb71a
polpenarroja <ppenarroja@nikel.es>
2021-12-14 10:02:21 +0100

    mostrar text y items en la mimsa linea

--- lib/templates/list/views/mobile.dart
+++ lib/templates/list/views/mobile.dart
@@ -85,21 +85,22 @@ class MobileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
+    return Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
       if (!haveConnection) NotConnectionWidget(text: text),
-      if (registros != null)
-        Container(
-          alignment: Alignment.centerRight,
-          padding: const EdgeInsets.all(8),
-          child: Text('$registros Items',
-              style: const TextStyle(color: Colors.grey, fontSize: 12)),
-        ),
-      if (textTopOfList != null)
-        Container(
-          padding: const EdgeInsets.all(8),
-          child: Text('$textTopOfList',
-              style: const TextStyle(color: Colors.grey, fontSize: 12)),
-        ),
+      if (registros != null || textTopOfList != null)
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Text('$registros Items',
+                style: const TextStyle(color: Colors.grey, fontSize: 12)),
+          ),
+          if (textTopOfList != null)
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Text('$textTopOfList',
+                  style: const TextStyle(color: Colors.grey, fontSize: 12)),
+            ),
+        ]),
       Expanded(
           child: CustomScrollView(
         controller: scrollController,
